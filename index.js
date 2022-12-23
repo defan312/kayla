2b3f2ce479243dfd6d677eec3bddd01489ccc40f
defan312 <106823339+defan312@users.noreply.github.com>
2022-12-23 11:10:30 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const T = json.attrs.t
 kayla.sendNode({
   tag: 'call',
     attrs: {
-      from: '6285798145596@s.whatsapp.net',
+      from: '6281321776496@s.whatsapp.net',
       id: Id,
       t: T
     },
